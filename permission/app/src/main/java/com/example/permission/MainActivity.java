9bbcc669de8dc124bb32e27d82ecdacc64aa644a
Ikhwanpulungan <37628655+Ikhwanpulungan@users.noreply.github.com>
2019-12-28 15:44:19 +0700

    Add files via upload

---
+++ permission/app/src/main/java/com/example/permission/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.permission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+}
